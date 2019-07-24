a2a8c997924ff519fab5187371fda7940eaa3cb7
jahanzebnaeem <jahanzeb.naeem@hotmail.com>
2019-07-24 07:27:26 -0700

    Orientation Class Code
    This is the code for orientation class.

---
+++ PNY.ConsoleApp/PNY.ConsoleApp.Batch17/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PNY.ConsoleApp.Batch17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            int b = number();
+            Console.WriteLine(a+b);
+        }
+
+        static int number()
+        {
+            return 6;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ASPNet_Batch17
+Repository for code shareing with PNY ASP.NET Batch 17 Students.
